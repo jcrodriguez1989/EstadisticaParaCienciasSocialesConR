52273b1d05895f34e66d9193fd6d6f0dd7a449ed
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2019-10-20 04:34:40 -0300

    trabajando en Parte 2

--- depencias.R
+++ depencias.R
@@ -7,8 +7,10 @@ load_install <- function(pkg, inst_fun = install.packages) {
   }
 }
 
+load_install("agricolae")
 load_install("bookdown")
 load_install("datasauRus")
+load_install("doBy")
 load_install("dplyr")
 load_install("ggplot2")
 load_install("ggthemes")
@@ -16,5 +18,10 @@ load_install("grid")
 load_install("gridExtra")
 load_install("knitr")
 load_install("kableExtra")
+load_install("magrittr")
 load_install("psych")
+load_install("pwr")
+load_install("questionr")
 load_install("tibble")
+load_install("splitstackshape")
+load_install("svglite")
